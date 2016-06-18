271ca77bb28683f5853cbddbe01110e96a23a9e0
jeking <jeking@ITLT005.rjobrien.com>
2016-06-18 00:01:07 -0500

    First Commit... Hello, asshole!

---
+++ HelloAsshole/HelloAsshole.java
@@ -0,0 +1,13 @@
+// Jimmy King
+// 2016-06-17
+// 
+// Reinstalling Eclipse & JDKs for the road ahead!
+
+public class HelloAsshole {
+	
+	public static void main (String[] args) {
+		
+		// The world can burn...
+		System.out.println("Hello, asshole!");
+	}
+}
